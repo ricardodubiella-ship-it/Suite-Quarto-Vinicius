268ca00168936100a155380016bd0d6e0719d4ec
ricardodubiella-ship-it <ricardo.dubiella@gmail.com>
2025-09-19 08:02:07 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_r02125---eduardo-gonalves---3d2---int__suite_qaurto-vinicius_r1_2025-09-18-12-00-49",
+      "name": "Panorama_R.021.25 - EDUARDO GONÇALVES - 3D2 - INT__SUITE_Qaurto Vinicius_R1_2025-09-18-12-00-49",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6481721114716237,
+        "pitch": -0.011805074999777787,
+        "fov": 1.3133145937252788
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2929769860284317,
+          "pitch": 0.035776443803019475,
+          "rotation": 0,
+          "target": "1-panorama_r02125---eduardo-gonalves---3d2---int__suite_qaurto-vinicius_r1_2025-09-18-12-11-23"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama_r02125---eduardo-gonalves---3d2---int__suite_qaurto-vinicius_r1_2025-09-18-12-11-23",
+      "name": "Panorama_R.021.25 - EDUARDO GONÇALVES - 3D2 - INT__SUITE_Qaurto Vinicius_R1_2025-09-18-12-11-23",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.651106984268531,
+          "pitch": 0.02138421099064125,
+          "rotation": 0,
+          "target": "0-panorama_r02125---eduardo-gonalves---3d2---int__suite_qaurto-vinicius_r1_2025-09-18-12-00-49"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
